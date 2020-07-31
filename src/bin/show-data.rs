47ea47a741cc7d7919a620e6f7ea2b378216a0b3
Jonathan Virga <jonathan.virga@gmail.com>
2020-08-01 00:08:20 +0200

    Update `show-data` binary to print last value of database.

--- src/bin/show-data.rs
+++ src/bin/show-data.rs
@@ -1,24 +1,27 @@
 extern crate diesel;
 extern crate teleinfo_reader;
 
-use self::teleinfo_reader::*;
-// use self::models;
 use self::diesel::prelude::*;
+use self::teleinfo_reader::*;
 use teleinfo_reader::models::Record;
 
 fn main() {
     use teleinfo_reader::schema::teleinfo::dsl::*;
 
     let connection = establish_connection();
-    let results = teleinfo.load::<Record>(&connection)
+    let results = teleinfo
+        .order_by(dt_utc.desc())
+        .limit(1)
+        .load::<Record>(&connection)
         .expect("Error loading data");
 
-    let plr = match results.len() {
-        0 | 1 => "",
-        _ => "s"
-    };
-    println!("=> {} row{}", results.len(), plr);
-    for record in results {
-        println!("{:?}", record);
+    if results.len() > 0 {
+        println!("Last values saved in database:");
+        for record in results {
+            println!("{}", record);
+        }
+    }
+    else {
+        println!("There's no data in database.")
     }
 }
